1032bd582691d04548af35368c3459ee68866844
Plat01 <fplatoff@gmail.com>
2023-08-28 20:27:22 +0400

    Try to add "GLEW"

--- main.cpp
+++ main.cpp
@@ -1,52 +1,104 @@
 #include <glm/glm.hpp>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 
-int main() {
-  if(!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-    return -1;
-  }
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout(location = 0) in vec3 aPos;
 
-  glfwDefaultWindowHints();
+    uniform mat4 projection;
 
-  GLFWwindow* window = glfwCreateWindow(300, 300, "Red Triangle", nullptr, nullptr);
-  if(window == nullptr) {
-    std::cerr << "Failed to open GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
+    void main()
+    {
+        gl_Position = projection * vec4(aPos, 1.0);
+    }
+)";
 
-  glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
-  glfwShowWindow(window);
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
 
-  glEnable(GL_DOUBLEBUFFER);
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LESS);
+    void main()
+    {
+        FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
+    }
+)";
 
-  glClearColor(0, 0, 0, 1);
+GLuint compileShader(GLenum type, const char* source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
 
-  glm::mat4 m = glm::perspective(45.0f, 4.0f / 3.0f, 1.0f, 100.0f);
-  glMatrixMode(GL_PROJECTION);
-  glLoadMatrixf(glm::value_ptr(m));
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cerr << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-  while(glfwWindowShouldClose(window) == GL_FALSE) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor4f(1, 0, 0, 1);
-    glBegin(GL_TRIANGLES);
-    glVertex3f( 0,  0.5, -5);
-    glVertex3f( 0.5, -0.5, -5);
-    glVertex3f(-0.5, -0.5, -5);
-    glEnd();
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+    return shader;
 }
+
+int main() {
+    // [Initialization and window creation code remains unchanged]
+
+    // Compile shaders
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+    // Link shaders to create a program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Vertex data for the triangle
+    float vertices[] = {
+            0.0f,  0.5f, -5.0f,
+            0.5f, -0.5f, -5.0f,
+            -0.5f, -0.5f, -5.0f
+    };
+
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 1.0f, 100.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    // [Window destruction and termination code remains unchanged]
+}
+
